23d2454ceecb25da51e94cd1e18fac557bd25608
LingXiaoWY <guoweiyelai@qq.com>
2022-07-12 12:12:40 +0800

    增加读写锁，提升了运行速度

--- src/thread_create.c
+++ src/thread_create.c
@@ -6,6 +6,8 @@
 =============================================================*/
 #include<thread_copy.h>
 
+pthread_rwlock_t lock;
+
 struct jpack
 {
 	const char*sfile,*dfile;
@@ -14,27 +16,17 @@ struct jpack
 
 void* jobs(void* arg)
 {
-	//pthread_detach(pthread_self());
-	//struct jpack* pjp = (struct jpack*)arg;
-	//printf("blocksize = %d,flag = %d\n",pjp->blocksize,pjp->flag);
-	/*char str_blocksize[50];
-	char str_pos[50];
-	bzero(str_blocksize,sizeof(str_blocksize));
-	bzero(str_pos,sizeof(str_pos));
-	sprintf(str_blocksize,"%d",pjp->blocksize);
-	sprintf(str_pos,"%d",pos);*/
-	//sleep(1);
-	printf("Copy thread tid 0x:%x Copy_Pos %d Copy_size %d\n",(unsigned int)pthread_self(),pjp->pos,pjp->blocksize);
-	//通过重载方法使用copy功能
-	//./copy sfile dfile blocksize pos
-	//execl("/home/guo/linuxHighLevel/Pthread/multithread_copy/module/copy","copy",pjp->sfile,pjp->dfile,str_blocksize,str_pos,NULL);//重载后覆盖
+	pthread_rwlock_wrlock(&lock);
+	struct jpack* pjp = (struct jpack*)arg;
+	printf("Copy thread tid 0x:%x Copy_Pos %d Copy_size %d\n",(unsigned int)pthread_self(),pjp->pos,pjp->blocksize);	
 	copy(pjp->sfile,pjp->dfile,pjp->blocksize,pjp->pos);
-	sleep(1);
+	pthread_rwlock_unlock(&lock);
 	return NULL;
 }
 
 int thread_create(const char* sfile,const char* dfile,int blocksize,int thrnum)
 {
+	pthread_rwlock_init(&lock,NULL);
 	pthread_t tid[110] = {0};
 	int flag;
 	int ret;
@@ -48,9 +40,9 @@ int thread_create(const char* sfile,const char* dfile,int blocksize,int thrnum)
 		//printf("blocksize = %d,flag=%d\n",blocksize,flag);
 		ret = pthread_create(&tid[flag],NULL,jobs,(void*)&jp);
 		if(ret)printf("thread create failed:%s\n",strerror(ret));
-		sleep(1);
+		usleep(1000);
 	}
-	//sleep(1);
+	//手动回收
 	for(flag=0;flag<thrnum;flag++){
 		pthread_cancel(tid[flag]);
 		if(pthread_join(tid[flag],&result)==0){
@@ -58,5 +50,6 @@ int thread_create(const char* sfile,const char* dfile,int blocksize,int thrnum)
 		}
 	}
 	printf("main thread join end...\n");
+	pthread_rwlock_destroy(&lock);
 	return 0;
 }
